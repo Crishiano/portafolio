18e5aad3615d3439f0d667b6c3117e5e25c3efa1
Crishiano <cristhiano.16.09@gmail.com>
2026-03-20 23:06:52 -0500

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,13 @@
   .col2{
     display: none;
   }*/
-
+const col2 = document.querySelector("#nav");
+    const menu = document.querySelector("#menu");
+    const header = document.querySelector("#header");
+    menu.addEventListener("click", () => {
+      col2.classList.toggle("visible");
+      header.classList.toggle("visible");
+    })
    
   const imagenes = [
   "imagenes/proyecto_clinica.png",
